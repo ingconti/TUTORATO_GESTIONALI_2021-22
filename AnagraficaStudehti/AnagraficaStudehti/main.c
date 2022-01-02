a1ea39001da16a65fdf23506c3968fac58cb1a38
ing.conti <ingconti@ingconti.com>
2022-01-02 12:29:17 +0100

    1st commit

---
+++ AnagraficaStudehti/AnagraficaStudehti/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AnagraficaStudehti
+//
+//  Created by ing.conti on 02 JAN 2022.
+
+
+#include <stdio.h>
+
+// logica:
+// scommentare i vari step, ma uno SOLO x volta.
+// il main e' GIA nei file inclusi
+
+#include "main1.h"
+
---
+++ AnagraficaStudehti/AnagraficaStudehti/main1.h
@@ -0,0 +1,13 @@
+//
+//  main1.c
+//  AnagraficaStudehti
+//
+//  Created by ing.conti on 02 JAN 2022.
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
