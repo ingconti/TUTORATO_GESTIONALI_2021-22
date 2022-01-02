dc29b398035b391ffef6c502f3a0c5332d7ab36f
ing.conti <ingconti@ingconti.com>
2022-01-02 16:54:51 +0100

    1' phase main1

--- AnagraficaStudehti/AnagraficaStudehti/main1.h
+++ AnagraficaStudehti/AnagraficaStudehti/main1.h
@@ -4,10 +4,40 @@
 //
 //  Created by ing.conti on 02 JAN 2022.
 
+//protos:
+
+bool leggiFilePrincipale(void);
+bool leggiFileSecondario(void);
+void stampaDati(void);
+
 
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    if (leggiFilePrincipale()){
+        if (leggiFileSecondario()){
+            stampaDati();
+        }
+    }
+    
     return 0;
 }
+
+// implementation:
+
+
+bool leggiFilePrincipale(void){
+    return true;
+}
+bool leggiFileSecondario(void)
+{
+    return true;
+}
+
+void stampaDati(void)
+{
+    printf("stampa dati\n");
+}
+
+
+
