173bc29cac67e3170acca0e9bc714f6255ade80f
ing.conti <ingconti@ingconti.com>
2022-01-02 16:53:41 +0100

    1st phase

--- AnagraficaStudehti/AnagraficaStudehti/main.c
+++ AnagraficaStudehti/AnagraficaStudehti/main.c
@@ -6,10 +6,13 @@
 
 
 #include <stdio.h>
+#include <stdbool.h>
+
 
 // logica:
 // scommentare i vari step, ma uno SOLO x volta.
 // il main e' GIA nei file inclusi
 
+//#include "main0.h"
 #include "main1.h"
 
